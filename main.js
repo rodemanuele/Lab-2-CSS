73a6327ce6531a5e162d3d7b69b888f97e330d81
rodemanuele <rodemanuele@gmail.com>
2025-08-13 14:29:57 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!"); 
+    }
+}
+
+
+
+
 document.addEventListener('DOMContentLoaded', function() {
         const button = document.getElementById('myButton');
         button.addEventListener('click', function() {
